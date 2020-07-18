b658c48020645ec32a2a9edf901459cafc7a89d0
han1ya <haniya.zafar@g.austincc.edu>
2020-07-18 03:44:28 +0000

    completed homework 6

--- src/classwork/10_assign/engineer.cpp
+++ src/classwork/10_assign/engineer.cpp
@@ -1,1 +1,9 @@
 //cpp
+#include "engineer.h"
+
+double Engineer::get_pay()
+{
+    gross_pay = base_pay + bonus_pay;
+
+    return gross_pay;
+}
--- test/classwork_test/10_assign_test/10_assign_tests.cpp
+++ test/classwork_test/10_assign_test/10_assign_tests.cpp
@@ -1,6 +1,33 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "employee.h"
+#include "engineer.h"
+#include "sales_employee.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
+
+TEST_CASE("Test class function Engineer::get_pay()")
+{
+	Employee* engineer1 = new Engineer(1500, 400);		
+	REQUIRE(engineer1->get_pay() == 1900);
+
+	delete engineer1;	
+	engineer1 = nullptr;
+}
+
+TEST_CASE("Test Engineer class function get_pay")
+{
+	Employee *engineer = new Engineer(1500, 400);
+	double test_result = engineer -> get_pay();
+	REQUIRE(test_result == 1900);
+}
+
+
+TEST_CASE("Test SalesEmployee class function get_pay")
+{
+	Employee *sales_employee = new SalesEmployee(40, 10, 500);
+	double test_result = sales_employee -> get_pay();
+	REQUIRE(test_result == 900);
+} 
